935982f331ffb4ea1f897b93d617d02789ddf50f
Nandini <115989171+nayandiniii@users.noreply.github.com>
2024-02-20 08:26:12 +0530

    Update 268. Missing Number.py

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,5 +1,5 @@
 class Solution(object):
     def missingNumber(self, nums):
-        
+        #create a set with index range and subtract from the nums itself to get the number that doesn not exist in nums
         for s in set(range(len(nums) + 1)) - set(nums):
             return s
