caa379df9cb6482cffb97f1fd942b171ba26fa64
Nandini <115989171+nayandiniii@users.noreply.github.com>
2024-02-23 12:36:22 +0530

    Update 1716. Calculate Money in Leetcode Bank.py

--- 1716. Calculate Money in Leetcode Bank.py
+++ 1716. Calculate Money in Leetcode Bank.py
@@ -3,11 +3,11 @@ class Solution(object):
         ans =0
         mon_count = 0
 
-        for i in range(1,n+1):
-            day = i%7
-            if day%7==0:
+        for i in range(1,n+1): #in the range of number of days given
+            day = i%7 #1=monday, 2=tuesday and so on..
+            if day%7==0: #if the day is sunday
                 day += 7 #increments day by 7 to represent monday
-            ans += mon_count+day
-            if i%7==0:
+            ans += mon_count+day #since every day the count if increementing
+            if i%7==0: #if it is a sunday
                 mon_count+=1
         return ans
