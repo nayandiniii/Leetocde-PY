95a9d05d70b87f44cdfbacf5f35f2cafa788a5b3
Nandini <115989171+nayandiniii@users.noreply.github.com>
2024-02-29 18:26:44 +0530

    Update 343. Integer Break.py

--- 343. Integer Break.py
+++ 343. Integer Break.py
@@ -3,7 +3,9 @@ class Solution(object):
         
         if (n==2 or n==3):
             return (n-1)
-        
+        '''
+        we subtract and multiply 3 because it yeilds the maximum output
+        '''
         res = 1
         while n>4:
             n = n-3
